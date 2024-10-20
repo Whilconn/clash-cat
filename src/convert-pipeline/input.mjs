54c50833368c51b61076ef305aabac26c80ddf5e
liuweikang <liuweikang@xiao100.com>
2024-10-20 19:42:00 +0800

    chore: update

--- src/convert-pipeline/input.mjs
+++ src/convert-pipeline/input.mjs
@@ -2,18 +2,20 @@ import path from 'node:path';
 import fsp from 'node:fs/promises';
 
 import { PATHS } from '../utils/constant.mjs';
+import { exec } from '../utils/exec.mjs';
 
 async function loadLocalFiles() {
   const tempDir = path.resolve(PATHS.localAbs, new Date().toJSON().slice(0, 10));
   const filePaths = await fsp.readdir(tempDir);
-  return filePaths.map((f) => path.resolve(tempDir, p));
+  return filePaths.map((f) => path.resolve(tempDir, f));
 }
 
 async function downloadRemoteFiles(url) {
-  await axios.get(url).then((res) => res.data);
+  await exec(`curl -O ${url}`);
+  await exec(`curl -O ${url}`);
 }
 
 export async function loadSubFiles() {
-  await downloadRemoteFiles();
+  // await downloadRemoteFiles('https://github.com/Whilconn/clash-cat/releases/download/v1/sub.zip');
   return loadLocalFiles();
 }
