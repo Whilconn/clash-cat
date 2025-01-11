4441d501930f1f144f9234aab6f7ffe2ae558b0b
liuweikang <liuweikang@xiao100.com>
2025-01-11 18:13:30 +0800

    chore: update

--- libs/libs.mjs
+++ libs/libs.mjs
@@ -1,20 +1,28 @@
 import os from 'node:os';
 
 const platform = os.type().toLowerCase();
+const host = 'bgithub.xyz';
 
 export const LIB_LITE = {
   zipName: 'lite.gz',
   appPath: './lite',
-  downloadUrl: `https://bgithub.xyz/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-${platform}-amd64-v3-v0.15.0.gz`,
+  downloadUrl: `https://${host}/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-${platform}-amd64-v3-v0.15.0.gz`,
   unzipCmd: 'gzip -d',
 };
 
 export const LIB_SUBCONVERTER = {
   zipName: 'subconverter.tar.gz',
   appPath: './subconverter/subconverter',
-  downloadUrl: `https://bgithub.xyz/tindy2013/subconverter/releases/download/v0.9.0/subconverter_${platform}64.tar.gz`,
+  downloadUrl: `https://${host}/tindy2013/subconverter/releases/download/v0.9.0/subconverter_${platform}64.tar.gz`,
   unzipCmd: 'tar -zxf',
   generateIniPath: 'subconverter/generate.ini',
 };
 
-export const LIBS = [LIB_LITE, LIB_SUBCONVERTER];
+export const LIB_MIHOMO = {
+  zipName: 'mihomo.gz',
+  appPath: './mihomo',
+  downloadUrl: `https://${host}/MetaCubeX/mihomo/releases/download/v1.19.1/mihomo-${platform}-amd64-v1.19.1.gz`,
+  unzipCmd: 'gzip -d',
+};
+
+export const LIBS = [LIB_LITE, LIB_SUBCONVERTER, LIB_MIHOMO];
